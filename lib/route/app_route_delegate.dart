dcd45b3afea848329024d8a5e3bab6f9d7306330
itning <itning@itning.top>
2021-10-04 00:17:05 +0800

    refactor: 重构路由

--- lib/route/app_route_delegate.dart
+++ lib/route/app_route_delegate.dart
@@ -1,71 +1,62 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:yunshu_music/page/music_list/music_list_page.dart';
 import 'package:yunshu_music/route/app_route_path.dart';
 
 /// 路由
 class AppRouterDelegate extends RouterDelegate<AppRoutePath>
     with ChangeNotifier, PopNavigatorRouterDelegateMixin<AppRoutePath> {
   @override
-  final GlobalKey<NavigatorState> navigatorKey;
+  GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-  AppRouterDelegate() : navigatorKey = GlobalKey<NavigatorState>() {
-    NavigatorHelper.get().init((widget) {
-      List<MaterialPage<dynamic>> page = [..._pages];
-      page.add(_generatePage(widget));
-      _pages = page;
-      notifyListeners();
-    });
+  final List<AppRoutePath> _stack = [];
+
+  List<String> get stack => List.unmodifiable(_stack);
+
+  static AppRouterDelegate of(BuildContext context) {
+    final delegate = Router.of(context).routerDelegate;
+    assert(delegate is AppRouterDelegate, 'Delegate type must match');
+    return delegate as AppRouterDelegate;
   }
 
-  static List<MaterialPage<dynamic>> _pages = [
-    _generatePage(const MusicListPage()),
-  ];
+  void push(String newRoute) {
+    _stack.add(AppRoutePath.createPage(newRoute));
+    notifyListeners();
+  }
+
+  void remove(String routeName) {
+    _stack.remove(AppRoutePath.createPage(routeName));
+    notifyListeners();
+  }
+
+  @override
+  AppRoutePath? get currentConfiguration =>
+      _stack.isNotEmpty ? _stack.last : null;
+
+  bool _onPopPage(Route<dynamic> route, dynamic result) {
+    if (_stack.isNotEmpty) {
+      if (_stack.last.path == route.settings.name) {
+        _stack.remove(_stack.last);
+        notifyListeners();
+      }
+    }
+    return route.didPop(result);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Navigator(
       key: navigatorKey,
-      pages: _pages,
-      onPopPage: (route, result) {
-        if (!route.didPop(result)) {
-          return false;
-        }
-        _pages.removeLast();
-        return true;
-      },
+      pages: [
+        for (final item in _stack) item.generatePage(),
+      ],
+      onPopPage: _onPopPage,
     );
   }
 
   @override
-  Future<void> setNewRoutePath(AppRoutePath configuration) async {}
-
-  /// 生成一个Page
-  static MaterialPage _generatePage(Widget widget) {
-    return MaterialPage(key: ValueKey(widget.hashCode), child: widget);
-  }
-}
-
-typedef OnJumpTo = void Function(Widget widget);
-
-class NavigatorHelper {
-  static NavigatorHelper? _instance;
-
-  static NavigatorHelper get() {
-    _instance ??= NavigatorHelper._();
-    return _instance!;
-  }
-
-  NavigatorHelper._();
-
-  OnJumpTo? _onJumpTo;
-
-  void init(OnJumpTo onJumpTo) {
-    _onJumpTo = onJumpTo;
-  }
-
-  void push(Widget widget) {
-    if (null != _onJumpTo) {
-      _onJumpTo!(widget);
-    }
+  Future<void> setNewRoutePath(AppRoutePath configuration) {
+    _stack.clear();
+    _stack.add(configuration);
+    return SynchronousFuture(null);
   }
 }
