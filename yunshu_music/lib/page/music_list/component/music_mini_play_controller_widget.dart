06a2be84d23b55e8818ec29fe9ecbebc0c41f765
itning <itning@itning.top>
2022-05-24 14:20:17 +0800

    refactor: use go_router instead original router

--- yunshu_music/lib/page/music_list/component/music_mini_play_controller_widget.dart
+++ yunshu_music/lib/page/music_list/component/music_mini_play_controller_widget.dart
@@ -1,13 +1,13 @@
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:tuple/tuple.dart';
 import 'package:yunshu_music/component/rotate_cover_image_widget.dart';
 import 'package:yunshu_music/net/model/music_entity.dart';
 import 'package:yunshu_music/provider/music_data_model.dart';
 import 'package:yunshu_music/provider/play_status_model.dart';
-import 'package:yunshu_music/route/app_route_delegate.dart';
 import 'package:yunshu_music/util/common_utils.dart';
 
 /// 小型音乐控制器Widget
@@ -31,7 +31,7 @@ class MusicMiniPlayControllerWidget extends StatelessWidget {
       child: SizedBox(
         height: 60.0,
         child: InkWell(
-          onTap: () => AppRouterDelegate.of(context).push('/musicPlay'),
+          onTap: () => context.go('/musicPlay'),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
