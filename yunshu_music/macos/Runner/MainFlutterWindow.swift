1ade0f99899c7ab08785e69798d9417fde472115
itning <itning@itning.top>
2022-05-25 11:12:19 +0800

    feat: 调整窗口大小

--- yunshu_music/macos/Runner/MainFlutterWindow.swift
+++ yunshu_music/macos/Runner/MainFlutterWindow.swift
@@ -6,7 +6,8 @@ class MainFlutterWindow: NSWindow {
     let flutterViewController = FlutterViewController.init()
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+    //self.setFrame(windowFrame, display: true)
+    self.setFrame(NSRect(x:0, y:0, width: 1200, height: 900), display: true)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
