6a2ab357a7e970f7184bb4ff0d28fbca50e5236c
itning <itning@itning.top>
2022-05-25 14:12:51 +0800

    feat: mac min max window

--- yunshu_music/macos/Runner/AppDelegate.swift
+++ yunshu_music/macos/Runner/AppDelegate.swift
@@ -6,4 +6,13 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return false
   }
+
+  override func applicationShouldHandleReopen(_: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+    if !flag {
+        for window: AnyObject in NSApplication.shared.windows {
+            window.makeKeyAndOrderFront(self)
+        }
+    }
+    return true
+  }
 }
