3f5172bdeeda3d0cf5b9a1e80bfff58d0464c75c
itning <itning@itning.top>
2022-06-09 10:10:05 +0800

    feat: init ios platform support

--- yunshu_music/windows/flutter/generated_plugin_registrant.cc
+++ yunshu_music/windows/flutter/generated_plugin_registrant.cc
@@ -6,22 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <dart_vlc/dart_vlc_plugin.h>
-#include <flutter_native_view/flutter_native_view_plugin.h>
-#include <music_channel_windows/music_channel_windows_plugin.h>
 #include <system_tray/system_tray_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 #include <window_manager/window_manager_plugin.h>
 #include <window_size/window_size_plugin.h>
-#include <windows_taskbar/windows_taskbar_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  DartVlcPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("DartVlcPlugin"));
-  FlutterNativeViewPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterNativeViewPlugin"));
-  MusicChannelWindowsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("MusicChannelWindowsPlugin"));
   SystemTrayPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SystemTrayPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
@@ -30,6 +20,4 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("WindowManagerPlugin"));
   WindowSizePluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("WindowSizePlugin"));
-  WindowsTaskbarPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("WindowsTaskbarPlugin"));
 }
