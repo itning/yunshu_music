e58e8d37ce1f450f72d2263fcde8a27a81cdcb29
itning <itning@itning.com>
2023-03-31 12:06:39 +0800

    build: update lib

--- music_channel_macos/lib/music_channel_macos.dart
+++ music_channel_macos/lib/music_channel_macos.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:math';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:music_platform_interface/music_model.dart';
