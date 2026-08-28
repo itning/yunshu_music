aed310ea468f9c9f219f8592f68ba83bbab53e6a
itning <itning@itning.com>
2026-08-28 22:53:34 +0800

    chore: 升级依赖并修复 Windows 平台问题
    - flutter pub upgrade --major-versions: go_router 18 / package_info_plus 10 / shimmer 4 / motion_toast 2.15
    - 钉死 flutter_rust_bridge 2.11.1，与 smtc_windows 内置 codegen 版本对齐
    - vendor audioplayers_windows（含上游 PR #1961 线程修复 + MSVC 编译错误修复），解决非平台线程发送 EventSink 消息问题

--- yunshu_music/macos/Flutter/GeneratedPluginRegistrant.swift
+++ yunshu_music/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,6 @@ import Foundation
 import audioplayers_darwin
 import music_channel_macos
 import package_info_plus
-import path_provider_foundation
 import screen_retriever_macos
 import shared_preferences_foundation
 import tray_manager
@@ -19,7 +18,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   MusicChannelMacosPlugin.register(with: registry.registrar(forPlugin: "MusicChannelMacosPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   TrayManagerPlugin.register(with: registry.registrar(forPlugin: "TrayManagerPlugin"))
