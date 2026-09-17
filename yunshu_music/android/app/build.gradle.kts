0292a9d5ecbaca3c91981d195b64f30d1065765e
itning <itning@itning.com>
2026-09-17 22:18:17 +0800

    chore(android): 升级 Gradle 9.3.1 / AGP 9.1.0 / Kotlin 2.4.0
    - 版本对齐 Flutter 3.47.4 模板，消除 Gradle/AGP 弃用警告
    
    - 移除 android.enableJetifier（AGP 9 不再需要）
    
    - app Java 版本提升到 17，移除无用的 Kotlin Gradle Plugin
    
    - R8 默认规则改用 proguard-android-optimize.txt（AGP 9 强制要求）

--- yunshu_music/android/app/build.gradle.kts
+++ yunshu_music/android/app/build.gradle.kts
@@ -1,10 +1,8 @@
 import java.util.Properties
 import java.io.FileInputStream
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("com.android.application")
-    id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -21,14 +19,8 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     defaultConfig {
@@ -53,7 +45,7 @@ android {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             isShrinkResources = true
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
